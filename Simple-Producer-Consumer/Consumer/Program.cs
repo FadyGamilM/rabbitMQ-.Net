d0f24ae2f92a528c6ee557baa5eab9bfe8cf7ecb
fady <gamilfady605@gmail.com>
2022-10-06 19:21:02 +0200

    the first simple example is done

--- Simple-Producer-Consumer/Consumer/Program.cs
+++ Simple-Producer-Consumer/Consumer/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+//! (1) : Create a connection factory and give it the host name where our instance is running
+var factory = new ConnectionFactory{HostName = "localhost"};
+using var connection = factory.CreateConnection();
+
+//! (2) : Create the channel
+using var channel = connection.CreateModel();
+
+//! (3) : Declare the queue we are going to use
+channel.QueueDeclare(
+   queue: "letterbox", // queue name
+   durable: false,
+   exclusive: false,
+   autoDelete: false,
+   arguments: null
+);
+
+//! (4) define the consumer which will consume the message
+var consumer = new EventingBasicConsumer(channel);
+
+//! (5) call the callback when we recieve the message
+consumer.Received += (model, encodedMsg) => {
+   // get the body of the msg
+   var body = encodedMsg.Body.ToArray();
+   // decode it
+   var message = Encoding.UTF8.GetString(body); 
+   System.Console.WriteLine($"[RECIEVED MESSAGE] => {message}");
+};
+
+//! (6) Consume the message
+channel.BasicConsume(
+   queue: "letterbox",
+   autoAck: true,
+   consumer: consumer
+);
+
+//! (7) exit 
+Console.ReadKey();
--- Simple-Producer-Consumer/Producer/Program.cs
+++ Simple-Producer-Consumer/Producer/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+//! (1) : Create a connection factory and give it the host name where our instance is running
+var factory = new ConnectionFactory{HostName = "localhost"};
+using var connection = factory.CreateConnection();
+
+//! (2) : Create the channel
+using var channel = connection.CreateModel();
+
+//! (3) : Declare the queue we are going to use
+channel.QueueDeclare(
+   queue: "letterbox", // queue name
+   durable: false,
+   exclusive: false,
+   autoDelete: false,
+   arguments: null
+);
+
+//! (4) the message we want to publish
+var msg = "my third message";
+
+//! (5) encode the message 
+var encodedMsg = Encoding.UTF8.GetBytes(msg);
+
+//! (6) publish the message to the default exchange for this example
+channel.BasicPublish("", "letterbox", null, encodedMsg);
+//                             "" -> default exchange
+//                             letterbox -> queue name
+
+System.Console.WriteLine($"[PUBLISHED MESSAGE] => {msg}");
