891ec25e0d9791c56623f05900fefa708b54c2d4
fady <gamilfady605@gmail.com>
2022-10-07 17:58:03 +0200

    header exchanges project

---
+++ Header-Exchanges/Consumer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+//! (1) : Create a connection factory and give it the host name where our instance is running
+var factory = new ConnectionFactory{HostName = "localhost"};
+using var connection = factory.CreateConnection();
+
+//! (2) : Create the channel
+using var channel = connection.CreateModel();
+
+//! (3) : Declare the exchange we are going to use
+channel.ExchangeDeclare(exchange: "headersExchange", type: ExchangeType.Headers);
+
+//! (4) : Declare the queue 
+var queue = channel.QueueDeclare(queue: "adminQueue");
+
+//! (5) : binding the queue to the exchange
+var bindingArgs = new Dictionary<string, object>{
+   {"x-match", "any"},
+   { "Email" , "admin@email.com" },
+   { "Username", "admin" }
+};
+
+channel.QueueBind(
+   queue: queue.QueueName,
+   exchange: "headersExchange",
+   routingKey: "",
+   arguments: bindingArgs
+);
+
+//! (6) define the consumer which will consume the message
+var consumer = new EventingBasicConsumer(channel);
+
+//! (7) call the callback when we recieve the message
+consumer.Received += (model, encodedMsg) => {
+   // get the body of the msg
+   var body = encodedMsg.Body.ToArray();
+   // decode it
+   var message = Encoding.UTF8.GetString(body); 
+   System.Console.WriteLine($"[RECIEVED MESSAGE] => {message}");
+};
+
+//! (8) Consume the message
+channel.BasicConsume(
+   queue: "adminQueue",
+   autoAck: true,
+   consumer: consumer
+);
+
+//! (9) exit 
+Console.ReadKey();
---
+++ Header-Exchanges/Producer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+//! (1) : Create a connection factory and give it the host name where our instance is running
+var factory = new ConnectionFactory{HostName = "localhost"};
+using var connection = factory.CreateConnection();
+
+//! (2) : Create the channel
+using var channel = connection.CreateModel();
+
+//! (3) : Declare the exchange we are going to use
+channel.ExchangeDeclare(exchange: "headersExchange", type: ExchangeType.Headers);
+
+//! (4) the message we want to publish
+var msg = "a message for the header exchange";
+
+//! (5) encode the message 
+var encodedMsg = Encoding.UTF8.GetBytes(msg);
+
+//! (6) publish the message, this message contains only 1 argument in the header so it will be routed to the queue who said "any"
+var props = channel.CreateBasicProperties();
+props.Headers = new Dictionary<string, object>{
+   { "Email" , "admin@email.com" }
+};
+channel.BasicPublish(
+   exchange: "headersExchange", 
+   routingKey : "", 
+   basicProperties: props, 
+   body: encodedMsg
+);
+System.Console.WriteLine($"[PUBLISHED MESSAGE] => {msg}");
+
+// ANOTHER MESSAGE TO THE QUEUE WHO SPECIFY "all"
+//! (4) the message we want to publish
+var msg2 = "another message for the header exchange";
+
+//! (5) encode the message 
+var encodedMsg2 = Encoding.UTF8.GetBytes(msg2);
+
+//! (6) publish the message, this message contains only 1 argument in the header so it will be routed to the queue who said "any"
+var props2 = channel.CreateBasicProperties();
+props2.Headers = new Dictionary<string, object>{
+   { "Email" , "admin@email.com" },
+   { "Username", "admin" }
+};
+channel.BasicPublish(
+   exchange: "headersExchange", 
+   routingKey : "", 
+   basicProperties: props2, 
+   body: encodedMsg2
+);
+System.Console.WriteLine($"[PUBLISHED MESSAGE] => {msg2}");
